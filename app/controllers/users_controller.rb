18e0612f8d87cbe9c6a9a521a2a2a81d680f4d1c
sebgomez2023 <sebgomez@uchicago.edu>
2023-02-13 22:23:49 +0000

    Finishing table for show in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def index_users
-    
+
     matching_users = User.all
     @list_of_users = matching_users.order({ :username => :asc})
 
     render({ :template => "user_templates/index_user.html.erb"})
+  end
+
+  def show_users
+
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => url_username})
+
+    @the_user = matching_usernames.at(0)
+
+    render({:template => "user_templates/show_user.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
 get("/users", {:controller => "users", :action => "index_users"})
 
-get("/users/:unique_name")
+get("/users/:path_username", {:controller => "users", :action => "show_users"})
 end

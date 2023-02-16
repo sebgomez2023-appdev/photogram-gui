7853d9f43c64f8fc9c99515a66b94b4065889a18
sebgomez2023 <sebgomez@uchicago.edu>
2023-02-16 23:27:34 +0000

    Finished Debugging

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -73,15 +73,16 @@ class PhotosController < ApplicationController
   def comment
     # input_author_id, input_body 
 
-    the_id = params.fetch("input_photo_id")
+    the_id = params.fetch("photo_id")
     the_author = params.fetch("input_author_id")
     the_comment = params.fetch("input_body")
 
     a_new_comment = Comment.new
-
-    a_new_comment.id = the_id
+    
+    
     a_new_comment.author_id = the_author
     a_new_comment.body = the_comment
+    a_new_comment.photo_id = the_id
 
     a_new_comment.save
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
+  get("/users", { :controller => "users", :action => "index_users" })
+  get("/users/:path_username", { :controller => "users", :action => "show_users" })
+  get("/", { :controller => "users", :action => "home" })
+  get("/insert_user_record", { :controller => "users", :action => "create_users" })
+  get("/update_user/:modify_user", { :controller => "users", :action => "update" })
 
-get("/users", {:controller => "users", :action => "index_users"})
-get("/users/:path_username", {:controller => "users", :action => "show_users"})
-get("/", {:controller => "users", :action => "home"})
-get("/insert_user_record", {:controller => "users", :action => "create_users"})
-get("/update_user/:modify_user", {:controller => "users", :action => "update"})
-
-
-get("/photos", {:controller => "photos", :action => "index_photos"})
-get("/photos/:path_id", {:controller => "photos", :action => "show_photos"})
-get("/delete_photo/:toast_id", {:controller => "photos", :action => "delete_photos"})
-get("/insert_photo", {:controller => "photos", :action => "create"})
-get("/update_photo/:modify_id", {:controller => "photos", :action => "update"})
-get("/insert_comment_record", {:controller => "photos", :action => "comment"})
+  get("/photos", { :controller => "photos", :action => "index_photos" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show_photos" })
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "delete_photos" })
+  get("/insert_photo", { :controller => "photos", :action => "create" })
+  get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
+  get("/insert_comment_record/:photo_id", { :controller => "photos", :action => "comment" })
 end
